8a416f0fcd2e59b09efe4e2e783035dfc15ebdab
trevorlyman <trevorlyman94@gmail.com>
2017-08-24 20:32:22 -0600

    Added parsing of the raw headers.

--- HttpServer/Request/Request.go
+++ HttpServer/Request/Request.go
@@ -2,22 +2,61 @@ package Request
 
 import (
 	//"../Response"
+	"strings"
 )
 
 type Request struct {
 	raw []byte
+	valid bool
+	rType string
+	headersMap map[string]string
+
 	path  string
 }
 
 func NewRequest(raw []byte) Request {
 	r := Request{}
 	r.raw = raw
+	r.valid = true
 	r.parseRaw()
 	return r
 }
 
 func (r Request) parseRaw() {
-	// Todo: Parse the raw byte input
-	r.path = "/"
+	s := string(r.raw)
+	s = strings.Replace(s, "\r", "", -1)
+	headers := strings.Split(s, "\n")
+
+	// Get request type
+	if strings.Contains(headers[0], "GET") {
+		r.rType = "GET"
+	} else if strings.Contains(headers[0], "POST") {
+		r.rType = "POST"
+	} else if strings.Contains(headers[0], "HEAD") {
+		r.rType = "HEAD"
+	} else {
+		r.valid = false
+		return
+	}
+
+	// Get path
+	path := strings.Split(s, " ")[1]
+	if (strings.Index(path, "/") == 0) { // path must start with '/'
+		r.path = path
+	}
+
+	// Map all other headers
+	for _, v := range headers[1:] {
+		if v == "" {
+			break
+		} // Stop when headers are no longer found
+		keyEnd := strings.Index(v, ":")
+		valueStart := strings.Index(v, " ") + 1
+
+		key := v[:keyEnd]
+		value := v[valueStart:]
+
+		r.headersMap[key] = value
+	}
 }
 
